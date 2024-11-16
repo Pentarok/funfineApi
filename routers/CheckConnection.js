40952a8d42ed041b48080952e7b307ee8f52046b
Pentarok <makpentarok@gmail.com>
2024-11-17 00:48:40 +0300

    server

--- routers/CheckConnection.js
+++ routers/CheckConnection.js
@@ -4,11 +4,12 @@ async function checkDbConnection(req, res) {
   try {
     if (req.method === 'OPTIONS') {
       res.status(200).end()
-      return
+      
     }
   
     await mongoose.connect(localUri, {
-      
+      connectTimeoutMS: 30000,
+      serverSelectionTimeoutMS: 30000  
     });
     res.status(200).json({ connected: true });
   } catch (error) {
